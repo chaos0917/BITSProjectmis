6e23d5735012896d96e3f8f6191aabea3f441c75
sjcra <sjcra@SC>
2025-12-10 02:09:47 -0800

    finished RESTful services

---
+++ Bits.Api/Controllers/RecipesController.cs
@@ -0,0 +1,48 @@
+﻿using Bits.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bits.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly BitsContext _context;
+
+        public RecipesController(BitsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/recipes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<object>> GetRecipe(int id)
+        {
+            var recipe = await _context.Recipes
+                .Where(r => r.RecipeId == id)
+                .Select(r => new
+                {
+                    r.RecipeId,
+                    r.Name,
+
+                    Ingredients = r.RecipeIngredients
+                        .Select(ri => new
+                        {
+                            ri.IngredientId,
+                            IngredientName = ri.Ingredient.Name,
+                            
+                        })
+                        .ToList()
+                })
+                .FirstOrDefaultAsync();
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(recipe);
+        }
+    }
+}
---
+++ Bits.Api/Program.cs
@@ -0,0 +1,38 @@
+﻿using Bits.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;   
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
+// ✅ register DbContext with DI container
+var conn = builder.Configuration.GetConnectionString("BitsConnection");
+builder.Services.AddDbContext<BitsContext>(options =>
+    options.UseMySql(conn, ServerVersion.AutoDetect(conn)));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
